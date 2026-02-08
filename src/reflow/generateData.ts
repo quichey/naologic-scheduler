cfa9536b4ef8a41ed9b8945255c6e1e04a91b50d
quichey <kylengo357@gmail.com>
2026-02-08 11:42:27 -0800

    initial packaging

--- src/reflow/generateData.ts
+++ src/reflow/generateData.ts
@@ -1,4 +1,4 @@
-import { WorkOrder, WorkCenter, ManufacturingOrder } from './types';
+import { WorkOrder, WorkCenter, ManufacturingOrder } from './types.js';
 import { v4 as uuidv4 } from 'uuid';
 import * as fs from 'fs';
 
